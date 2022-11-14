c2cdd6cdf2dadc7a9f75f3a6ee3ac6e1ad072da7
naheeeee <fpqghd@naver.com>
2022-11-14 10:31:19 +0900

    221114 수정

--- common.js
+++ common.js
@@ -9,16 +9,3 @@ $(function () {
     },
   });
 });
-
-// 900px
-/* <script>
-var swiper1 = new Swiper(".review", {
-  slidesPerView: 4,
-  spaceBetween: 30,
-  loop: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
-</script> */
